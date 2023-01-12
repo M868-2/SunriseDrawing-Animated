472fb21a126035b733606e5cd9aab0984982a804
M868-2 <113375937+M868-2@users.noreply.github.com>
2023-01-12 11:15:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,6 @@ cnv.height = 600;
 // IMAGE VARIABLE
 let cloud = document.getElementById("cloud-img");
 
-// VARIABLES
-let mouseIsClicked = false;
-
 // BACKGROUNG COLOUR
 ctx.fillStyle = "Blue";
 ctx.fillRect(0, 0, 800, 400);
@@ -21,94 +18,41 @@ ctx.fillRect(0, 400, 800, 200);
 // GLOBAL VARIABLES
 let circRed = 255;
 let circBlue = 0;
-let circGreeb = 0;
+let circGreen = 0;
+let sunHeight = 400;
+let sunRadius = 50;
+let sunStartAng = Math.PI;
+let sunEndAng = 0;
+let cloud1X = 350;
+let cloud2X = 400;
 
-// DRAW HALF CIRCLE
-ctx.lineWidth = 2;
-ctx.fillStyle = "rgb(255 , 0, 0)";
-ctx.beginPath();
-ctx.arc(400, 400, 50, Math.PI, 0);
-ctx.fill();
+requestAnimationFrame(loop);
+function loop() {
+  sunHeight -= 5;
+  sunRadius += 2;
+  sunStartAng += Math.PI;
+  sunEndAng += 2 * Math.PI;
+  circBlue += 2;
+  cloud1X += 2;
+  cloud2X -= 2;
 
-// CLOUD IMAGE
-ctx.drawImage(cloud, 350, 175);
-ctx.drawImage(cloud, 400, 150);
+  // BACKGROUND COLOUR
+  ctx.fillStyle = "Blue";
+  ctx.fillRect(0, 0, 800, 400);
 
-// requestAnimationFrame(loop);
+  ctx.fillStyle = "Green";
+  ctx.fillRect(0, 400, 800, 200);
 
-// function loop() {
-//   if (mouseIsPressed) {
-//     let circRed = 255;
-//     cloud++;
-//     circRed -= 5;
-//   }
-// }
+  // DRAW HALF CIRCLE
+  ctx.lineWidth = 2;
+  ctx.fillStyle = "rgb(" + circRed + " ," + circBlue + " , " + circGreen + ")";
+  ctx.beginPath();
+  ctx.arc(400, sunHeight, sunRadius, sunStartAng, sunEndAng);
+  ctx.fill();
 
-// // EVENTS
-// document.addEventListener("mousedown", mouseDownHandler);
-// document.addEventListener("mouseup", mouseUpHandler);
+  // CLOUD IMAGE
+  ctx.drawImage(cloud, cloud1X, 175);
+  ctx.drawImage(cloud, cloud2X, 150);
 
-// function mouseDownHandler() {}
-
-// DRAWING BASICS
-// STORE IMAGES IN VARIABLES
-// let gucciLogo = document.getElementById("gucci-logo");
-
-// // LET'S DRAW SOMETHING USING THE GRAPHICS COMNTEX (CTX)
-// // DRAW RECTANGLES
-// // SET THE OUTLINE AND FILL COLOURS
-// // USE ANY VALID CSS COLOURS: NAME, RGB (), RGBA (), HEX #FF0033
-// ctx.strokeStyle = "purple";
-// ctx.strokeRect(50, 20, 150, 50);
-
-// ctx.fillStyle = "pink";
-// ctx.fillRect(225, 150, 200, 200);
-
-// // DRAW TEXT
-// ctx.font = "40px Times New Roman";
-// ctx.fillStyle = "black";
-// ctx.fillText("I Found You :)", 300, 50);
-
-// ctx.font = "30px Arial";
-// ctx.strokeStyle = "rgb(0, 20, 40)";
-// ctx.strokeText("Save me", 300, 100);
-
-// // DRAW LINES
-// ctx.lineWidth = 2;
-// ctx.strokeStyle = "blue";
-// ctx.beginPath();
-// ctx.moveTo(500, 200);
-// ctx.lineTo(700, 150);
-// ctx.lineTo(600, 100);
-// ctx.stroke();
-
-// // DRAW POLYGON
-// ctx.lineWidth = 2;
-// ctx.fillStyle = "cyan";
-// ctx.beginPath();
-// ctx.moveTo(500, 400);
-// ctx.lineTo(700, 350);
-// ctx.lineTo(600, 300);
-// ctx.closePath();
-// ctx.fill();
-
-// // DRAW CIRCLES
-// ctx.lineWidth = 2;
-// ctx.strokeStyle = "black";
-// ctx.beginPath();
-// ctx.arc(100, 500, 50, 0, 2 * Math.PI);
-// ctx.stroke();
-
-// ctx.lineWidth = 2;
-// ctx.fillStyle = "grey";
-// ctx.beginPath();
-// ctx.arc(250, 500, 30, 0, 2 * Math.PI);
-// ctx.fill();
-
-// ctx.fillStyle = "cyan";
-// ctx.beginPath();
-// ctx.arc(250, 500, 20, 0, 2 * Math.PI);
-// ctx.fill();
-
-// // DRAW IMAGES
-// ctx.drawImage(gucciLogo, 300, 400);
+  requestAnimationFrame(loop);
+}
